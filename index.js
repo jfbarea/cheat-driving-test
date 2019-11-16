72d9645fb3683b54157c7cfea39bba1241132617
Fran Barea <jfcobarea@gmail.com>
2019-11-16 01:03:45 +0100

    answering all test question but test is not finished

--- index.js
+++ index.js
@@ -13,10 +13,26 @@ function getDataId (rawAttrs, attr='id') {
   return rawAttrs.match(pattern)[1];
 }
 
+function getToken (html) {
+  const pattern =  /token = "([A-Za-z0-9]*)"/
+  return html.match(pattern)[1];
+}
+
+function answerQuestion(testId, _token, questionId, selectedResponse){
+  return api.post('check-questions', {
+    testId,
+    _token,
+    questions: [{
+      questionId,
+      selectedResponse,
+      selectedKnowledge: 'S',
+    }]
+  });
+}
 
 async function main () {
-  const {headers, data} = await api.get('/realizar-test');
-  const document = parse(data);
+  const { headers, data: HTMLString } = await api.get('/realizar-test');
+  const document = parse(HTMLString);
   const test = document.querySelector('div.test-item-container');
   const testId = getDataId(test.rawAttrs);
 
@@ -24,11 +40,16 @@ async function main () {
   const questionIds = questions.map(e => getDataId(e.rawAttrs));
 
   const answers = test.querySelectorAll('div.question-response');
-  const answerIds = answers.map(e => getDataId(e.rawAttrs, 'data'));
-  const orderedAnswerIds = answerIds.reduce((acc, cur, i) => {
-    console.log(questionIds[i]);
-  }, {});
-  console.log(orderedAnswerIds);
+  const answerIds = answers.map(e => getDataId(e.rawAttrs, 'data'))
+    .filter((id, index) => index % 3 ===0);
+  const token = getToken(HTMLString);
+  console.log(answerIds.length);
+  console.log(headers);
+  console.log(token);
+  const postResponse = await Promise.all(questionIds.map((questionId, index) => {
+    return answerQuestion(testId, token, questionId, answerIds[index]);
+  }));
+  console.log('POST RESPONSE', postResponse);
 }
 
 main();
